c7588ea52747674cf8888c703ec2d0307d067e94
jenil18 <jenilgajera19@gmail.com>
2025-06-10 19:26:50 +0530

    update app

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ uploaded_file = st.file_uploader("Upload an image of the toothbrush", type=["jpg
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_container_width=True)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
     
     prediction = predict(image)
     # st.write(prediction)
